337a6fa80f104622154fbde130f41473e01d3e0b
U-BSD\292270 <292270@SHS-10DCE47FA.beaverton.k12.or.us>
2017-09-19 11:17:28 -0700

    This is the final update/completed version

--- palindrome.cpp
+++ palindrome.cpp
@@ -12,17 +12,37 @@ int main() {
     char in [80];
     cin.getline(in, 80);
     for (int i = 0; i < strlen(in); i++){
-      
+      in[i] = tolower(in[i]);
     }
     int size = strlen(in) - 1;
     char pali [80];
+    char spaceless [80];
+    int count = 0;
+    int scount = 0;
     for (int i = 0; i <= size; i++) {
-      if (!(isspace(in[size-1]))){
-	pali [i] = in [size - i];
-	cout << pali [i];
+      if (!isspace(in[size-i]) && !ispunct(in[size-i])){
+	pali [i - count] = in [size - i];
+      }
+      else{
+	count++;
+      }
+      if (!isspace(in[i]) && !ispunct(in[i])){
+	spaceless [i - scount] = in [i];
+      }
+      else {
+	scount ++;
       }
     }
-    
+    pali [size - count + 1] = '\0';
+    spaceless [size - count + 1] = '\0';
+    cout << (size - count + 1) << endl;
+    cout << pali << endl;
+    cout << spaceless << endl;
+    if (strcmp(pali,spaceless) == 0) {
+      cout << "This is a palindrome" << endl;
+    }
+    else {
+      cout << "This is not a palindrome" << endl;
+    }
   }
-
 }
